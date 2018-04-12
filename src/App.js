ee3bbb6b97fe078d5e7bec08a1f133efe8aabce6
ivy <zhengyumeng1125@gmail.com>
2018-04-12 16:47:10 -0700

    moved left banner state to App

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      hideBannerClass:"",
+      leftBannerClass: "banner-cover",
+      leftImageClass:'left-face',
+      leftShowChart:false,
+      leftShowBackground:true,
       hideRightBannerClass:""
     }
     this.toggleLeftImage = this.toggleLeftImage.bind(this)
-    this.toggleRightImage = this.toggleRightImage.bind(this)
+    this.showChartPage = this.showChartPage.bind(this)
     this.showLeftPage = this.showLeftPage.bind(this)
   }
 
@@ -23,16 +26,27 @@ class App extends Component {
     })
   }
 
-  toggleRightImage() {
+  showChartPage() {
     this.setState({
-      hideRightBannerClass:"banner-right-cover-disappear"
+      hideRightBannerClass:"banner-right-cover-disappear",
+      leftShowBackground:false,
+      leftBannerClass: 'banner-cover banner-cover-click',
+      leftImageClass: 'left-face left-face-click'
     })
+    setTimeout(() => {
+      this.setState({
+        leftShowChart:true
+      })
+    },2000)
   }
 
   showLeftPage() {
     this.setState({
-      hideBannerClass:"banner-cover-click",
-      hideRightBannerClass:"banner-right-cover-disappear"
+      leftBannerClass:"banner-cover banner-cover-click",
+      hideRightBannerClass:"banner-right-cover-disappear",
+      leftImageClass: 'left-face left-face-click',
+      leftShowBackground:false,
+      leftShowChart:true
     })
   }
 
@@ -41,8 +55,19 @@ class App extends Component {
       <div className="App">
         <Nav />
         <div className="banner">
-          <BannerLeft show={this.state.showLeftImage}  toggleRightImage={this.toggleRightImage} hideBannerClass={this.state.hideBannerClass}/>
-          <BannerRight show={this.state.showRightImage} toggleLeftImage={this.toggleLeftImage} bannerRightCoverDisappear={this.state.hideRightBannerClass}  showLeftPage={this.showLeftPage} />
+          <BannerLeft 
+          show={this.state.showLeftImage}  
+          showChartPage={this.showChartPage} 
+          leftBannerClass={this.state.leftBannerClass}
+          leftImageClass={this.state.leftImageClass}
+          leftShowChart={this.state.leftShowChart}
+          leftShowBackground={this.state.leftShowBackground}
+          />
+          <BannerRight 
+          show={this.state.showRightImage} 
+          toggleLeftImage={this.toggleLeftImage} 
+          bannerRightCoverDisappear={this.state.hideRightBannerClass}  
+          showLeftPage={this.showLeftPage} />
         </div>
       </div>
     );
--- src/BannerLeft.js
+++ src/BannerLeft.js
@@ -6,48 +6,33 @@ import DesignChart from './DesignChart.js';
 class BannerLeft extends Component {
   constructor(props){
     super(props)
-    this.state = {
-      bannerClass: 'banner-cover',
-      imageClass: 'left-face',
-      showChart: false
-    }
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick() {
-     this.setState({
-       bannerClass: 'banner-cover banner-cover-click ',
-       imageClass: 'left-face left-face-click'
-     })
-     this.props.toggleRightImage()
-     setTimeout(()=>{
-      this.setState({
-        showChart:true
-      })
-     },1500)
-     $('#ps').fadeOut(1500)
-     $('#ai').fadeOut(1500)
-     $('#id').fadeOut(1500)
-     $('#designer').fadeOut(1500)
+     this.props.showChartPage()
   }
 
 
-
   render() {
-    console.log(this.state.bannerClass+" "+this.props.hideBannerClass)
     let designChart;
-    if(this.state.showChart){
+    if(this.props.leftShowChart){
       designChart = <DesignChart />
     }
-
+    if(!this.props.leftShowBackground){
+      $('#ps').fadeOut(1500)
+      $('#ai').fadeOut(1500)
+      $('#id').fadeOut(1500)
+      $('#designer').fadeOut(1500)
+    }
     return (
-      <div className={this.state.bannerClass+" "+this.props.hideBannerClass} onClick={this.handleClick}>
+      <div className={this.props.leftBannerClass} onClick={this.handleClick}>
           <span id="designer" className="word">Designer</span>
           <img src="images/ps.png" alt="ps" id="ps" className="image"></img>
           <img src="images/ai.png" alt="ai" id="ai" className="image"></img>
           <img src="images/id.png" alt="id" id="id" className="image"></img>
           {designChart}
-          <img src="images/left-face.png" alt="left-face" className={this.state.imageClass} id="left-face"></img>
+          <img src="images/left-face.png" alt="left-face" className={this.props.leftImageClass} id="left-face"></img>
       </div>
     );
   }

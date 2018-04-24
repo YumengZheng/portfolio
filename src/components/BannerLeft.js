4ccc29b620bdb7395f040cffb3e0ca9d1bbdd6fa
ivy <zhengyumeng1125@gmail.com>
2018-04-24 13:28:56 -0700

    add mouse hover

--- src/components/BannerLeft.js
+++ src/components/BannerLeft.js
@@ -8,9 +8,13 @@ class BannerLeft extends Component {
   constructor(props){
     super(props)
     this.state = {
-      clickHandleOn: true
+      clickHandleOn: true,
+      onHover: false
     }
     this.handleClick = this.handleClick.bind(this)
+    this.handleMouseEnter = this.handleMouseEnter.bind(this)
+    this.handleMouseLeave = this.handleMouseLeave.bind(this)
+    this.constrolBackground = this.constrolBackground.bind(this)
   }
 
   handleClick() {
@@ -18,6 +22,48 @@ class BannerLeft extends Component {
       this.props.showLeftChartPage()
       this.setState({clickHandleOn:false})
       }
+  }
+
+  handleMouseEnter(){
+    this.setState({
+      onHover: true
+    })
+  }
+
+  handleMouseLeave(){
+    this.setState({
+      onHover: false
+    })
+  }
+
+  constrolBackground(){
+    let background
+    let fontColor
+    this.state.onHover? fontColor = {color: 'white', position: 'absolute'} : fontColor = {color: 'black'}
+    if(this.props.leftShowBackground){
+      background =[<span id="designer" style={fontColor} className="word">Designer</span>,
+                  <img src="images/ps.png" alt="ps" id="ps" className="image"></img>,
+                  <img src="images/ai.png" alt="ai" id="ai" className="image"></img>,
+                  <img src="images/id.png" alt="id" id="id" className="image"></img>]
+    }
+    return background
+  }
+
+  controlToolTip(){
+    let toolTip
+    this.state.onHover? toolTip = {display: 'block', color: 'red', position: 'fixed'} : toolTip = {display: 'none'}
+    return <div id='tooltip' style={toolTip} title="regular tooltip">Hover me</div>
+  }
+
+  findTooltipPosition() {
+    var tooltipSpan = document.getElementById('tooltip');
+    window.onmousemove = function (e) {
+        var x = e.screenX,
+            y = e.screenY;
+            console.log(x,y)
+        tooltipSpan.style.top = (y-40) + 'px';
+        tooltipSpan.style.left = x + 'px';
+    }
   }
 
   render() {
@@ -31,20 +77,23 @@ class BannerLeft extends Component {
       $('#id').fadeOut(1500)
       $('#designer').fadeOut(1500)
     }
-    let background
-    if(this.props.leftShowBackground){
-      background =[<span id="designer" className="word">Designer</span>,
-                  <img src="images/ps.png" alt="ps" id="ps" className="image"></img>,
-                  <img src="images/ai.png" alt="ai" id="ai" className="image"></img>,
-                  <img src="images/id.png" alt="id" id="id" className="image"></img>]
+    let findTooltipPosition
+    if(this.state.onHover){
+      findTooltipPosition = this.findTooltipPosition()
     }
     return (
-      <div className={this.props.leftBannerClass} onClick={this.handleClick} id="left-banner">
-          {background}
-          {designChart}
-          <img src="images/left-face2.png" alt="left-face" className={this.props.leftImageClass} id="left-face"></img>
+      <div className={this.props.leftBannerClass} 
+           onClick={this.handleClick} 
+           onMouseEnter={this.handleMouseEnter} 
+           onMouseLeave={this.handleMouseLeave}
+           id="left-banner" >
+           {this.controlToolTip()}
+           {findTooltipPosition}
+           {this.constrolBackground()}
+           {designChart}
+           <img src="images/left-face2.png" alt="left-face" className={this.props.leftImageClass} id="left-face"></img>
       </div>
-    );
+    )
   }
 }
 

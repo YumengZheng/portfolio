ff430e1a701a94a5a690d53f1208cd23646549e7
ivy <zhengyumeng1125@gmail.com>
2018-05-03 00:11:02 -0700

    emma page responsive

--- src/components/EmmaDesign.js
+++ src/components/EmmaDesign.js
@@ -7,8 +7,19 @@ class EmmaDesign extends Component {
     super(props)
   }
   render() {
+    const width = document.body.clientWidth 
+    let height;
+    if(700<=width){
+      height = {height: width*2}
+    } else if(500<width && width<=700){
+      height = {height: width*3}
+    } else if(0<=width && width<=500){
+      console.log('pass')
+      height = {height: width*3.35}
+    }
+console.log(width)
     return (
-      <div className="emma-design">
+      <div className="emma-design" style={height}>
         <img src='./images/emma.png' className="emma-design-image"/>
       </div>
     );

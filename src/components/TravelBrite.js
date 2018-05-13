98da7d0ff10f2113db5468d1fd932e28ef69b9c9
ivy <zhengyumeng1125@gmail.com>
2018-05-13 12:53:19 -0700

    change travelbrite discription

--- src/components/TravelBrite.js
+++ src/components/TravelBrite.js
@@ -30,7 +30,7 @@ class TravelBrite extends Component {
 
     return (
       <div className="travelbrite" style={height}>
-        <div className='travelbrite-text-box'><p className='travelbrite-text'><span className='travelbrite-text-name'>TravelBrite</span> is a back-end project I worked on for a traveling website. With the goal of designing the back-end to function smoothly at web-scale, I started the project with seeding 10 million data in MongoDB and Postgres to test the most the suitable database. I then implemented Redis as well as connected Mongo pool to decrease the query time. I used New Relic and artillery.io to stress test my architecture at every step and deployed the project on AWS.
+        <div className='travelbrite-text-box'><p className='travelbrite-text'><span className='travelbrite-text-name'>TravelBrite</span>is a project I worked on with the goal of designing a back-end to handle 1000 requests per second. I started the project by seeding my 10 million rows of questions and answer data into MongoDB and Postgres, in order to test which database would perform the fastest for my needs.  I implemented Redis as well as connected Mongo pool to decrease the query time. I used New Relic and artillery.io to stress test my architecture at every step and deployed the project on AWS. 
         <br></br><br></br><span style={{fontWeight: 'bold'}}>Technology used in the project:</span><br></br> <span style={{color:'#44A685'}}>Node.js, Express, MongoDB, Mongoose, Postgres, Pg-promise, Docker, Redis, Newrelic, Redis</span></p></div>
         <div className='travelbrite-image-box'><img src='./images/travel-brite-big1.png' className='travelbrite-image'/></div>
       </div>
